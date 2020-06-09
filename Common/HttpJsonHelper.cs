ef34d671ac22b41e05bd2b5d0733ab9ceed81e85
周世林 <295754922@qq.com>
2020-06-09 16:33:17 +0800

    update

--- Common/HttpJsonHelper.cs
+++ Common/HttpJsonHelper.cs
@@ -11,41 +11,101 @@ namespace Common
     /// </summary>
     public static class HttpJsonHelper
     {
+        /// <summary>
+        /// post异步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="action"></param>
+        /// <param name="content"></param>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
         public static Task<HttpResponseMessage> HttpPostAsync(string url, string action, HttpContent content, string bearerToken = "")
         {
             return GetHttpClient(url).AddAuthorizationHeader(bearerToken).PostAsync(action, content.AddJsonContentType());
         }
 
+        /// <summary>
+        /// Post同步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="action"></param>
+        /// <param name="content"></param>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
         public static HttpResponseMessage HttpPost(string url, string action, HttpContent content, string bearerToken = "")
         {
             return HttpPostAsync(url, action, content, bearerToken).Result;
         }
 
+        /// <summary>
+        /// get异步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="action"></param>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
         public static Task<HttpResponseMessage> HttpGetAsync(string url, string action, string bearerToken = "")
         {
             return GetHttpClient(url).AddAuthorizationHeader(bearerToken).GetAsync(action);
         }
 
+        /// <summary>
+        /// get同步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="action"></param>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
         public static HttpResponseMessage HttpGet(string url, string action, string bearerToken = "")
         {
             return HttpGetAsync(url, action, bearerToken).Result;
         }
 
+        /// <summary>
+        /// put异步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="action"></param>
+        /// <param name="content"></param>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
         public static Task<HttpResponseMessage> HttpPutAsync(string url, string action, HttpContent content, string bearerToken = "")
         {
             return GetHttpClient(url).AddAuthorizationHeader(bearerToken).PutAsync(action, content.AddJsonContentType());
         }
 
+        /// <summary>
+        /// put同步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="action"></param>
+        /// <param name="content"></param>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
         public static HttpResponseMessage HttpPut(string url, string action, HttpContent content, string bearerToken = "")
         {
             return HttpPutAsync(url, action, content, bearerToken).Result;
         }
 
+        /// <summary>
+        /// delete异步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="action"></param>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
         public static Task<HttpResponseMessage> HttpDeleteAsync(string url, string action, string bearerToken = "")
         {
             return GetHttpClient(url).AddAuthorizationHeader(bearerToken).DeleteAsync(action);
         }
 
+        /// <summary>
+        /// delete同步请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="action"></param>
+        /// <param name="bearerToken"></param>
+        /// <returns></returns>
         public static HttpResponseMessage HttpDelete(string url, string action, string bearerToken = "")
         {
             return HttpDeleteAsync(url, action, bearerToken).Result;
@@ -58,6 +118,12 @@ namespace Common
             return httpClient;
         }
 
+        /// <summary>
+        /// 异步获取请求结果
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="response"></param>
+        /// <returns></returns>
         public static async Task<T> GetResponseAsync<T>(HttpResponseMessage response)
         {
             if (response.StatusCode == HttpStatusCode.BadRequest || response.StatusCode == HttpStatusCode.PaymentRequired)
@@ -69,6 +135,12 @@ namespace Common
             return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync());
         }
 
+        /// <summary>
+        /// 同步获取请求结果
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="response"></param>
+        /// <returns></returns>
         public static T GetResponse<T>(HttpResponseMessage response)
         {
             return GetResponseAsync<T>(response).Result;
