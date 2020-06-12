1a0b4d4ff76398dd613fd9156520a0081e92832b
周世林 <295754922@qq.com>
2020-06-12 16:59:44 +0800

    update

--- Common.DAL.Transaction/TransactionResourceHelper.cs
+++ Common.DAL.Transaction/TransactionResourceHelper.cs
@@ -19,51 +19,48 @@ namespace Common.DAL.Transaction
 
         public static bool ApplayResource(Type table, long identity, int weight, int timeOut = EMPTY_TIME_OUT)
         {
-            return true;
-            //return ApplayResourceAsync(table, identity, weight, timeOut == EMPTY_TIME_OUT ? m_timeOut : timeOut).Result;
+            return ApplayResourceAsync(table, identity, weight, timeOut == EMPTY_TIME_OUT ? m_timeOut : timeOut).Result;
         }
 
         public static async Task<bool> ApplayResourceAsync(Type table, long identity, int weight, int timeOut = EMPTY_TIME_OUT)
         {
-            return true;
+            int time = Environment.TickCount;
 
-            //int time = Environment.TickCount;
+            bool result = await m_applyResourceProcessor.Apply(table, identity, weight, timeOut == EMPTY_TIME_OUT ? m_timeOut : timeOut);
 
-            //bool result = await m_applyResourceProcessor.Apply(table, identity, weight, timeOut == EMPTY_TIME_OUT ? m_timeOut : timeOut);
+            Console.WriteLine(Environment.TickCount - time);
 
-            //Console.WriteLine(Environment.TickCount - time);
-
-            //return result;
+            return result;
         }
 
         public static void ReleaseResource(Type table, long identity)
         {
-         //   ReleaseResourceAsync(table, identity).Wait();
+            ReleaseResourceAsync(table, identity).Wait();
         }
 
         public static async Task ReleaseResourceAsync(Type table, long identity)
         {
-            //int time = Environment.TickCount;
+            int time = Environment.TickCount;
 
-            //if (!await m_releaseResourceProcessor.Release(table, identity))
-            //    throw new DealException($"释放事务资源{table.FullName}失败。");
+            if (!await m_releaseResourceProcessor.Release(table, identity))
+                throw new DealException($"释放事务资源{table.FullName}失败。");
 
-            //Console.WriteLine(Environment.TickCount - time);
+            Console.WriteLine(Environment.TickCount - time);
         }
 
         static TransactionResourceHelper()
         {
-            //string timeOutString = ConfigManager.Configuration["ResourceManager:TimeOut"];
-            //m_timeOut = string.IsNullOrWhiteSpace(timeOutString) ? DEFAULT_TIME_OUT : Convert.ToInt32(timeOutString);
+            string timeOutString = ConfigManager.Configuration["ResourceManager:TimeOut"];
+            m_timeOut = string.IsNullOrWhiteSpace(timeOutString) ? DEFAULT_TIME_OUT : Convert.ToInt32(timeOutString);
 
-            //m_serviceClient = new ServiceClient(TransferAdapterFactory.CreateZeroMQTransferAdapter(new IPEndPoint(IPAddress.Parse(ConfigManager.Configuration["RPC:IPAddress"]), Convert.ToInt32(ConfigManager.Configuration["RPC:Port"])), ZeroMQSocketTypeEnum.Client, ConfigManager.Configuration["RPC:Identity"]), BufferSerialzerFactory.CreateBinaryBufferSerializer(Encoding.UTF8));
+            m_serviceClient = new ServiceClient(TransferAdapterFactory.CreateZeroMQTransferAdapter(new IPEndPoint(IPAddress.Parse(ConfigManager.Configuration["RPC:IPAddress"]), Convert.ToInt32(ConfigManager.Configuration["RPC:Port"])), ZeroMQSocketTypeEnum.Client, ConfigManager.Configuration["RPC:Identity"]), BufferSerialzerFactory.CreateBinaryBufferSerializer(Encoding.UTF8));
 
-            //m_serviceClient.Start();
+            m_serviceClient.Start();
 
-            //m_applyResourceProcessor = new ApplyResourceProcessor(m_serviceClient);
-            //m_releaseResourceProcessor = new ReleaseResourceProcessor(m_serviceClient);
+            m_applyResourceProcessor = new ApplyResourceProcessor(m_serviceClient);
+            m_releaseResourceProcessor = new ReleaseResourceProcessor(m_serviceClient);
 
-            //AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
         }
 
         private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
