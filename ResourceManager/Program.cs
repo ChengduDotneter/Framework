46a8082d5e743be729ded37029bebb909b00927e
语 轻 <2535476954@qq.com>
2020-06-17 14:25:39 +0800

    1

--- ResourceManager/Program.cs
+++ ResourceManager/Program.cs
@@ -6,8 +6,6 @@ using Common.RPC.TransferAdapter;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Orleans;
-using Orleans.Hosting;
 using System;
 using System.Net;
 using System.Text;
@@ -27,18 +25,6 @@ namespace ResourceManager
             serviceClient.Start();
 
             new HostBuilder()
-                //.UseOrleans((Microsoft.Extensions.Hosting.HostBuilderContext context, ISiloBuilder builder) =>
-                //{
-                //    builder
-                //        .UseLocalhostClustering()
-                //        .Configure<ClusterOptions>(options =>
-                //        {
-                //            options.ClusterId = "ResourceManager";
-                //            options.ServiceId = "ResourceManager";
-                //        })
-                //        .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
-                //        .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(Resource).Assembly).WithReferences());
-                //})
                 .ConfigureServices(services =>
                 {
                     services.Configure<ConsoleLifetimeOptions>(options =>
