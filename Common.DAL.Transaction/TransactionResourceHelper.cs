17543bfbb9bd039fa80f8cecd7ac8721b24d3a7d
语 轻 <2535476954@qq.com>
2020-06-17 14:27:26 +0800

    1

--- Common.DAL.Transaction/TransactionResourceHelper.cs
+++ Common.DAL.Transaction/TransactionResourceHelper.cs
@@ -90,94 +90,6 @@ namespace Common.DAL.Transaction
                 throw new DealException($"释放事务{identity}资源失败。");
         }
 
-
-
-
-        class TestAdapter : ITransferAdapter
-        {
-            public event OnBufferRecievedHandler OnBufferRecieved;
-            public event Action<SessionContext, byte[]> S;
-
-            public void SendBuffer(SessionContext sessionContext, byte[] buffer, int length)
-            {
-                byte[] d = new byte[length];
-                Array.Copy(buffer, d, d.Length);
-                S?.Invoke(sessionContext, d);
-            }
-
-            public void Strat()
-            {
-
-            }
-
-            public void Recieve(SessionContext sessionContext, byte[] d)
-            {
-                OnBufferRecieved?.Invoke(sessionContext, d);
-            }
-        }
-
-
-
-        /// <summary>
-        /// 申请资源处理器，服务发起端（现包括ZeroMQ和UDP）
-        /// </summary>
-        class AApplyResourceProcessor : ResponseProcessorBase<ApplyRequestData>
-        {
-            private const int MAX_TIME_OUT = 1000 * 60;
-            private ServiceClient m_serviceClient;
-
-            public AApplyResourceProcessor(ServiceClient serviceClient) : base(serviceClient)
-            {
-                m_serviceClient = serviceClient;
-            }
-
-            /// <summary>
-            /// 数据处理
-            /// </summary>
-            /// <param name="sessionContext"></param>
-            /// <param name="data"></param>
-            protected override async void ProcessData(SessionContext sessionContext, ApplyRequestData data)
-            {
-                //if (data.TimeOut < 0 ||
-                //    data.TimeOut > MAX_TIME_OUT)
-                //    throw new DealException($"超时时间范围为：{0}-{MAX_TIME_OUT}ms");
-
-                //IResource resource = m_actorClient.GetGrain<IResource>(data.ResourceName);
-                //bool successed = await resource.Apply(data.Identity, data.Weight, data.TimeOut);
-
-                //SendSessionData(m_serviceClient, sessionContext, new ApplyResponseData() { Success = successed });
-
-                SendSessionData(m_serviceClient, sessionContext, new ApplyResponseData() { Success = true });
-            }
-        }
-
-        /// <summary>
-        /// 释放资源处理器，服务发起端（现包括ZeroMQ和UDP）
-        /// </summary>
-        class AReleaseResourceProcessor : ResponseProcessorBase<ReleaseRequestData>
-        {
-            private ServiceClient m_serviceClient;
-
-            public AReleaseResourceProcessor(ServiceClient serviceClient) : base(serviceClient)
-            {
-                m_serviceClient = serviceClient;
-            }
-
-            /// <summary>
-            /// 数据处理
-            /// </summary>
-            /// <param name="sessionContext"></param>
-            /// <param name="data"></param>
-            protected override async void ProcessData(SessionContext sessionContext, ReleaseRequestData data)
-            {
-                //IResource resource = m_actorClient.GetGrain<IResource>(data.ResourceName);
-                //await resource.Release(data.Identity);
-                SendSessionData(m_serviceClient, sessionContext, new ReleaseResponseData());
-            }
-        }
-
-
-
         /// <summary>
         /// 静态构造函数
         /// </summary>
@@ -186,38 +98,16 @@ namespace Common.DAL.Transaction
             string timeOutString = ConfigManager.Configuration["ResourceManager:TimeOut"];
             m_timeOut = string.IsNullOrWhiteSpace(timeOutString) ? DEFAULT_TIME_OUT : Convert.ToInt32(timeOutString);
 
-            //var a = new TestAdapter();
-            //m_serviceClient = new ServiceClient(a, BufferSerialzerFactory.CreateBinaryBufferSerializer(Encoding.UTF8));
+            //m_serviceClient = new ServiceClient(TransferAdapterFactory.CreateUDPCRCTransferAdapter(new IPEndPoint(IPAddress.Parse(ConfigManager.Configuration["RPC:IPAddress"]), Convert.ToInt32(ConfigManager.Configuration["RPC:Port"])), UDPCRCSocketTypeEnum.Client), BufferSerialzerFactory.CreateBinaryBufferSerializer(Encoding.UTF8));
 
-            m_serviceClient = new ServiceClient(TransferAdapterFactory.CreateUDPCRCTransferAdapter(new IPEndPoint(IPAddress.Parse(ConfigManager.Configuration["RPC:IPAddress"]), Convert.ToInt32(ConfigManager.Configuration["RPC:Port"])), UDPCRCSocketTypeEnum.Client), BufferSerialzerFactory.CreateBinaryBufferSerializer(Encoding.UTF8));
-
-            //m_serviceClient = new ServiceClient(TransferAdapterFactory.CreateZeroMQTransferAdapter(new IPEndPoint(IPAddress.Parse(ConfigManager.Configuration["RPC:IPAddress"]), Convert.ToInt32(ConfigManager.Configuration["RPC:Port"])), ZeroMQSocketTypeEnum.Client, Guid.NewGuid().ToString()), BufferSerialzerFactory.CreateBinaryBufferSerializer(Encoding.UTF8));
-
-
+            m_serviceClient = new ServiceClient(TransferAdapterFactory.CreateZeroMQTransferAdapter(new IPEndPoint(IPAddress.Parse(ConfigManager.Configuration["RPC:IPAddress"]), Convert.ToInt32(ConfigManager.Configuration["RPC:Port"])), ZeroMQSocketTypeEnum.Client, Guid.NewGuid().ToString()), BufferSerialzerFactory.CreateBinaryBufferSerializer(Encoding.UTF8));
 
             m_applyResourceProcessor = new ApplyResourceProcessor(m_serviceClient);
             m_releaseResourceProcessor = new ReleaseResourceProcessor(m_serviceClient);
 
             AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
 
-
-
-            //var b = new TestAdapter();
-            //ServiceClient c = new ServiceClient(b, BufferSerialzerFactory.CreateBinaryBufferSerializer(Encoding.UTF8));
-            //AApplyResourceProcessor aApplyResourceProcessor = new AApplyResourceProcessor(c);
-            //AReleaseResourceProcessor aReleaseResourceProcessor = new AReleaseResourceProcessor(c);
-
-
-            //a.S += b.Recieve;
-            //b.S += a.Recieve;
-
-
-
             m_serviceClient.Start();
-            //c.Start();
-
-
-
         }
 
         /// <summary>
