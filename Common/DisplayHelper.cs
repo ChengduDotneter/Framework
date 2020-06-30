6070f4c180cbda88719d234fc5fa3eb9c25b8a00
语 轻 <2535476954@qq.com>
2020-06-30 13:03:15 +0800

    update

---
+++ Common/DisplayHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Common
+{
+    public class DisplayHelper
+    {
+        public static string GetDisplayName(object value)
+        {
+            object[] attrs = value.GetType().GetField(value.ToString()).GetCustomAttributes(typeof(DisplayAttribute), false);
+            if (attrs.Count() != 1)
+                throw new DealException("获取描述失败");
+
+            return ((DisplayAttribute)attrs[0]).Name;
+        }
+    }
+}
