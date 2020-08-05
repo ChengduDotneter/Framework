b855d358ed09b21c5c2f54a8a93a5a8a166682a9
周世林 <295754922@qq.com>
2020-08-05 10:12:40 +0800

    updat

---
+++ Common.Log/CustomLog.cs
@@ -0,0 +1,24 @@
+﻿using Apache.Ignite.Core.Cache.Configuration;
+using Common.Validation;
+using SqlSugar;
+
+namespace Common.Log
+{
+    public class CustomLog : LogViewModelBase
+    {
+        /// <summary>
+        /// 是否错误日志
+        /// </summary>
+        [SugarColumn(IsNullable = false, ColumnDescription = "是否错误日志")]
+        [QuerySqlField(NotNull = true)]
+        public bool IsError { get; set; }
+
+        /// <summary>
+        /// 日志
+        /// </summary>
+        [StringMaxLength(1000)]
+        [SugarColumn(IsNullable = false, ColumnDescription = "日志")]
+        [QuerySqlField(NotNull = true)]
+        public string Message { get; set; }
+    }
+}
---
+++ Common.Log/Log4netLogHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Common.Log
+{
+    public class Log4netLogHelper : ILogHelper
+    {
+        public void Error(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string path, string methed, string parameters, string controllerGroup, string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string path, string methed, string parameters, string controllerGroup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sql(string sql, string parameters = "", string message = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TCCNode(long transcationID, bool isError, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TCCServer(long transcationID, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
