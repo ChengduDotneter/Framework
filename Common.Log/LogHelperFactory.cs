1a1c235cf591ba6126fa0c6a5b2b8d5c98b04523
周世林 <295754922@qq.com>
2020-08-05 18:00:02 +0800

    update

---
+++ Common.Log/LogHelperFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace Common.Log
+{
+    public class LogHelperFactory
+    {
+        private static ILogHelper m_log4netLogHelper;
+        private static ILogHelper m_kafkaLogHelper;
+
+        static LogHelperFactory()
+        {
+            m_log4netLogHelper = new Log4netLogHelper();
+            m_kafkaLogHelper = new KafkaLogHelper();
+        }
+
+        public static ILogHelper GetLog4netLogHelper()
+        {
+            return m_log4netLogHelper;
+        }
+
+        public static ILogHelper GetKafkaLogHelper()
+        {
+            return m_kafkaLogHelper;
+        }
+    }
+}
