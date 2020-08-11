d75290af79a5d9302db5be839ea37fc3a9fadba4
周世林 <295754922@qq.com>
2020-08-11 17:46:56 +0800

    update

---
+++ Common.Log/CustomErrorLog.cs
@@ -0,0 +1,17 @@
+﻿using Common.MessageQueueClient;
+
+namespace Common.Log
+{
+    public class CustomErrorLog : LogViewModelBase
+    {
+        /// <summary>
+        /// 日志
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 自定义日志编码
+        /// </summary>
+        public string CustomCode { get; set; }
+    }
+}
