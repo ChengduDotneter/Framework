5ff598396d997bc343f5c581de10a777e433d05a
语 轻 <2535476954@qq.com>
2020-08-25 13:51:36 +0800

    update

--- Common/MD5Encryption.cs
+++ Common/MD5Encryption.cs
@@ -14,20 +14,26 @@ namespace Common
         /// <summary>
         /// 获取MD5加密字符串
         /// </summary>
-        /// <param name="password"></param>
+        /// <param name="encryptedString"></param>
         /// <param name="charset"></param>
         /// <returns></returns>
-        public static string GetMd5Password(string password, string charset)
+        public static string GetMD5(string encryptedString, string charset)
         {
-            if (string.IsNullOrWhiteSpace(password))
-                throw new NullReferenceException();
+            byte[] fromData = Encoding.GetEncoding(charset).GetBytes(encryptedString);
+            byte[] targetData = m_md5.ComputeHash(fromData);
+            string byte2String = null;
 
-            return Convert.ToBase64String(m_md5.ComputeHash(Encoding.GetEncoding(charset).GetBytes(password)));
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+            }
+
+            return byte2String;
         }
 
         static MD5Encryption()
         {
-            m_md5 = MD5.Create();
+            m_md5 = new MD5CryptoServiceProvider();
         }
     }
 }
