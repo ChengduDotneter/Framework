6c82a7ff3393ca7c70f7f965b9fe09ff535609cf
周世林 <295754922@qq.com>
2021-02-23 13:36:51 +0800

    UPDATE

---
+++ Common/HttpResponseExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Common
+{
+    public static class HttpResponseExtension
+    {
+        public static HttpResponse SetJsonContentType(this HttpResponse httpResponse)
+        {
+            return httpResponse.SetContentType("application/json");
+        }
+
+        public static HttpResponse SetXMLContentType(this HttpResponse httpResponse)
+        {
+            return httpResponse.SetContentType("application/xml");
+        }
+
+        public static HttpResponse SetHTMLContentType(this HttpResponse httpResponse)
+        {
+            return httpResponse.SetContentType("text/html;utf8");
+        }
+
+        internal static HttpResponse SetContentType(this HttpResponse httpResponse, string contentType)
+        {
+            httpResponse.ContentType = contentType;
+            return httpResponse;
+        }
+    }
+}
