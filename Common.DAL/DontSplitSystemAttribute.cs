7f2501a204eb42c91fc5a294fef448eb7bf5f8c5
张晓亚 <133110205@qq.com>
2021-11-11 16:20:43 +0800

    123

--- Common.DAL/DontSplitSystemAttribute.cs
+++ Common.DAL/DontSplitSystemAttribute.cs
@@ -1,10 +1,9 @@
 using System;
 
 namespace Common.DAL
-
 {
     /// <summary>
-    /// 不分表
+    /// 不分区特性
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
     public class DontSplitSystemAttribute : Attribute
